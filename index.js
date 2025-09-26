51790534347dad5e8d075f8f34c9344df378c8c2
VictorHugoGomesCassunde <victorhugocassunde10@gmail.com>
2025-09-26 00:02:42 -0400

    arrumado bugs

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+let contador = 0; 
 document.getElementById("btnContar").addEventListener("click", function() {
-  contador++; 
+  contador++;
   document.getElementById("contador").innerText = contador;
 });
+
+
 document.getElementById("btnInverter").addEventListener("click", function() {
   let texto = document.getElementById("textoOriginal").value;
   let invertido = texto.split("").reverse().join(""); 
-  document.getElementById("textoInvertido").innerText = "Invertido: " + invertidoo; 
+  document.getElementById("textoInvertido").innerText = "Invertido: " + invertido; 
   
 });
+
+
 document.getElementById("btnSomar").addEventListener("click", function() {
-  let n1 = document.getElementById("num1").value;
-  let n2 = document.getElementById("num2").value;
+  let n1 = parseFloat(document.getElementById("num1").value) || 0;
+  let n2 = parseFloat(document.getElementById("num2").value) || 0;
   let soma = n1 + n2; 
   document.getElementById("resultado").innerText = "Resultado: " + soma;
 });
