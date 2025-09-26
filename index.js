fcd42cb41f632e4f3f953aa443beab9b96387b9b
VictorHugoGomesCassunde <victorhugocassunde10@gmail.com>
2025-09-25 23:55:53 -0400

    bug erro digitação

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ document.getElementById("btnContar").addEventListener("click", function() {
   contador++; 
   document.getElementById("contador").innerText = contador;
 });
+document.getElementById("btnInverter").addEventListener("click", function() {
+  let texto = document.getElementById("textoOriginal").value;
+  let invertido = texto.split("").reverse().join(""); 
+  document.getElementById("textoInvertido").innerText = "Invertido: " + invertidoo; 
+  
+});
